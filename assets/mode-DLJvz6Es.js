fd80dccabfcf3c56b13322415336b6da9b713150
KongFFF <3192972081@qq.com>
2025-11-03 22:29:04 +0800

    Delete assets directory

--- assets/mode-DLJvz6Es.js
+++
@@ -1,1 +0,0 @@
-class t extends ui.view.DefaultTheme.ModeUI{constructor(){super(),this.btnCustom.on(Laya.Event.CLICK,this,()=>$ui.switchView(UI.pages.TALENT))}}export{t as default};
--- assets/mode-DQoleqwP.js
+++
@@ -1,1 +0,0 @@
-class s extends ui.view.CyberTheme.ModeUI{constructor(){super(),this.btnCustom.on(Laya.Event.CLICK,this,()=>$ui.switchView(UI.pages.TALENT))}}export{s as default};
